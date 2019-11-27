3430bea375d61c5f8386201b6c4b407ac18c319d
Williams Santos <willysantos_lai@gmail.com>
2019-11-27 11:52:34 -0600

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Nav/>
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Nav'
+  export default {
+    name: 'fronted-potras',
+    components: {Nav}
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
